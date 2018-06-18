7a8755def22febfdfa3e64d44d40ed8308b21e68
Juan Pereira <juanmanuel.pereira1@gmail.com>
2018-06-18 12:54:05 -0300

    Create files for trending repos
    - Create TrendingReposViewController class
    - Create TrendingReposViewController view
    - Add code to select the trending view as the initial screen
    - Delete default ViewController and Main.storyboard

---
+++ XapoiOSInterview/Stories/Trending Repos/TrendingReposViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TrendingReposViewController.swift
+//  XapoiOSInterview
+//
+//  Created by Juan Pereira on 6/18/18.
+//  Copyright © 2018 Juan Pereira. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TrendingReposViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: "TrendingReposViewController", bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) is not supported")
+    }
+}
